a7d06c15d1a7a0f6ecb77c51d9d5afd23f5c02a5
dandan.wu <1240172019@qq.com>
2017-01-22 17:52:51 +0800

    update

--- routes/signout.js
+++ routes/signout.js
@@ -8,8 +8,11 @@ var checkLogin = require('../middlewares/check').checkLogin;
 
 // GET /signout 登出
 router.get('/', checkLogin, function(req, res, next) {
-    console.log('signout');
-    res.send('signout');
+    // console.log('signout');
+    req.session.user = null;
+    req.flash('success','登出成功');
+    res.redirect('/posts');
+    // res.send('signout');
 });
 
 module.exports = router;
